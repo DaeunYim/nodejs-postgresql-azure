096a4c1306f03e8fe91998d00ef7423297a6db39
daeunyim <daeunyim@microsoft.com>
2021-06-02 22:13:44 -0700

    recover

--- project-1/sql/products.sql
+++ project-1/sql/products.sql
@@ -4,8 +4,3 @@ CREATE TABLE IF NOT EXISTS products (
     quantity INTEGER NOT NULL,
     price NUMERIC(5,2)
 );
-
-CREATE TABLE IF NOT EXISTS customers (
-    customerId SERIAL PRIMARY KEY,
-    customerName VARCHAR(255) NOT NULL
-);
