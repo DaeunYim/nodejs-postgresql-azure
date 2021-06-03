0a8ece01f6ba8621d92c944975672e4689328fb4
daeunyim <daeunyim@microsoft.com>
2021-06-02 18:07:39 -0700

    add feature and update sql file

--- project-1/sql/products.sql
+++ project-1/sql/products.sql
@@ -1,11 +1,14 @@
+DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS customers;
+
+CREATE TABLE customers (
+    customerId SERIAL PRIMARY KEY,
+    customerName VARCHAR(255) NOT NULL
+);
+
 CREATE TABLE products (
     productId SERIAL PRIMARY KEY,
     productName VARCHAR(255) NOT NULL,
     quantity INTEGER NOT NULL,
     price NUMERIC(5,2)
-);
-
-CREATE TABLE customers (
-    customerId SERIAL PRIMARY KEY,
-    customerName VARCHAR(255) NOT NULL
 );
